7d1bfaeb5c513cc187900d30997897bb843ba6a7
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-02-12 00:55:35 +0500

    header title ozgardi

--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -246,10 +246,12 @@ const Main = () => {
         <div className="slider-content">
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
-              <h2>Sayoxatni biz bilan mazmunli o'tkazing!</h2>
-              <p>
-                Eʼtiborga molik joylarni, insonlar hayoti va hodisalar to‘g‘risida eng qiziqarli maʼlumotlarni siz biz orqali topishingiz mumkin!
-              </p>
+              <h2 className="header-title-per">Sayoxatni biz bilan mazmunli o'tkazing!</h2>
+              <h3 className="header-title-simple">
+                Eʼtiborga molik joylarni, insonlar hayoti va hodisalar
+                to‘g‘risida eng qiziqarli maʼlumotlarni siz biz orqali
+                topishingiz mumkin!
+              </h3>
               {/* <div className="search-form wow pulse" data-wow-delay="0.8s">
                 <form action="" className="form-inline">
                   <div className="form-group">
