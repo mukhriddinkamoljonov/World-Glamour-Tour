7b1d4a742081d0c45110332f94a0540167de95b2
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-02-12 23:20:59 +0500

    sddg

--- src/Components/Pages/AllCart.jsx
+++ src/Components/Pages/AllCart.jsx
@@ -17,9 +17,10 @@ const AllCart = () => {
         class="content-area single-property"
         style={{ backgroundColor: "#fcfcfc" }}
       >
+        &nbsp;
         <div class="container">
           <div class="clearfix padding-top-40">
-            <div class="col-md-8 single-property-content">
+            <div class="col-md-8 single-property-content prp-style-1">
               <div class="row">
                 <div class="light-slide-item">
                   <div class="clearfix">
@@ -43,7 +44,7 @@ const AllCart = () => {
                       <li data-thumb="assets/img/property-1/property1.jpg">
                         <img src="assets/img/property-1/property1.jpg" />
                       </li>
-                      <li data-thumb="img/property-1/property2.jpg">
+                      <li data-thumb="assets/img/property-1/property2.jpg">
                         <img src="assets/img/property-1/property3.jpg" />
                       </li>
                       <li data-thumb="assets/img/property-1/property3.jpg">
@@ -66,22 +67,9 @@ const AllCart = () => {
                 </div>
 
                 <div class="property-meta entry-meta clearfix">
-                  <div class="col-xs-3 col-sm-3 col-md-3 p-b-15">
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
                     <span class="property-info-icon icon-tag">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 48 48"
-                      >
-                        <path
-                          class="meta-icon"
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
-                          fill="#FFA500"
-                          d="M47.199 24.176l-23.552-23.392c-.504-.502-1.174-.778-1.897-.778l-19.087.09c-.236.003-.469.038-.696.1l-.251.1-.166.069c-.319.152-.564.321-.766.529-.497.502-.781 1.196-.778 1.907l.092 19.124c.003.711.283 1.385.795 1.901l23.549 23.389c.221.218.482.393.779.523l.224.092c.26.092.519.145.78.155l.121.009h.012c.239-.003.476-.037.693-.098l.195-.076.2-.084c.315-.145.573-.319.791-.539l18.976-19.214c.507-.511.785-1.188.781-1.908-.003-.72-.287-1.394-.795-1.899zm-35.198-9.17c-1.657 0-3-1.345-3-3 0-1.657 1.343-3 3-3 1.656 0 2.999 1.343 2.999 3 0 1.656-1.343 3-2.999 3z"
-                        ></path>
-                      </svg>
+                      <img src="assets/img/icon/sale-orange.png" />
                     </span>
                     <span class="property-info-entry">
                       <span class="property-info-label">Status</span>
@@ -89,20 +77,9 @@ const AllCart = () => {
                     </span>
                   </div>
 
-                  <div class="col-xs-3 col-sm-3 col-md-3 p-b-15">
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
                     <span class="property-info icon-area">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 48 48"
-                      >
-                        <path
-                          class="meta-icon"
-                          fill="#FFA500"
-                          d="M46 16v-12c0-1.104-.896-2.001-2-2.001h-12c0-1.103-.896-1.999-2.002-1.999h-11.997c-1.105 0-2.001.896-2.001 1.999h-12c-1.104 0-2 .897-2 2.001v12c-1.104 0-2 .896-2 2v11.999c0 1.104.896 2 2 2v12.001c0 1.104.896 2 2 2h12c0 1.104.896 2 2.001 2h11.997c1.106 0 2.002-.896 2.002-2h12c1.104 0 2-.896 2-2v-12.001c1.104 0 2-.896 2-2v-11.999c0-1.104-.896-2-2-2zm-4.002 23.998c0 1.105-.895 2.002-2 2.002h-31.998c-1.105 0-2-.896-2-2.002v-31.999c0-1.104.895-1.999 2-1.999h31.998c1.105 0 2 .895 2 1.999v31.999zm-5.623-28.908c-.123-.051-.256-.078-.387-.078h-11.39c-.563 0-1.019.453-1.019 1.016 0 .562.456 1.017 1.019 1.017h8.935l-20.5 20.473v-8.926c0-.562-.455-1.017-1.018-1.017-.564 0-1.02.455-1.02 1.017v11.381c0 .562.455 1.016 1.02 1.016h11.39c.562 0 1.017-.454 1.017-1.016 0-.563-.455-1.019-1.017-1.019h-8.933l20.499-20.471v8.924c0 .563.452 1.018 1.018 1.018.561 0 1.016-.455 1.016-1.018v-11.379c0-.132-.025-.264-.076-.387-.107-.249-.304-.448-.554-.551z"
-                        ></path>
-                      </svg>
+                      <img src="assets/img/icon/room-orange.png" />
                     </span>
                     <span class="property-info-entry">
                       <span class="property-info-label">Area</span>
@@ -112,20 +89,9 @@ const AllCart = () => {
                     </span>
                   </div>
 
-                  <div class="col-xs-3 col-sm-3 col-md-3 p-b-15">
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
                     <span class="property-info-icon icon-bed">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 48 48"
-                      >
-                        <path
-                          class="meta-icon"
-                          fill="#FFA500"
-                          d="M21 48.001h-19c-1.104 0-2-.896-2-2v-31c0-1.104.896-2 2-2h19c1.106 0 2 .896 2 2v31c0 1.104-.895 2-2 2zm0-37.001h-19c-1.104 0-2-.895-2-1.999v-7.001c0-1.104.896-2 2-2h19c1.106 0 2 .896 2 2v7.001c0 1.104-.895 1.999-2 1.999zm25 37.001h-19c-1.104 0-2-.896-2-2v-31c0-1.104.896-2 2-2h19c1.104 0 2 .896 2 2v31c0 1.104-.896 2-2 2zm0-37.001h-19c-1.104 0-2-.895-2-1.999v-7.001c0-1.104.896-2 2-2h19c1.104 0 2 .896 2 2v7.001c0 1.104-.896 1.999-2 1.999z"
-                        ></path>
-                      </svg>
+                      <img src="assets/img/icon/bed-orange.png" />
                     </span>
                     <span class="property-info-entry">
                       <span class="property-info-label">Bedrooms</span>
@@ -133,20 +99,19 @@ const AllCart = () => {
                     </span>
                   </div>
 
-                  <div class="col-xs-3 col-sm-3 col-md-3 p-b-15">
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
+                    <span class="property-info-icon icon-bed">
+                      <img src="assets/img/icon/cars-orange.png" />
+                    </span>
+                    <span class="property-info-entry">
+                      <span class="property-info-label">Car garages</span>
+                      <span class="property-info-value">1</span>
+                    </span>
+                  </div>
+
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
                     <span class="property-info-icon icon-bath">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 48 48"
-                      >
-                        <path
-                          class="meta-icon"
-                          fill="#FFA500"
-                          d="M37.003 48.016h-4v-3.002h-18v3.002h-4.001v-3.699c-4.66-1.65-8.002-6.083-8.002-11.305v-4.003h-3v-3h48.006v3h-3.001v4.003c0 5.223-3.343 9.655-8.002 11.305v3.699zm-30.002-24.008h-4.001v-17.005s0-7.003 8.001-7.003h1.004c.236 0 7.995.061 7.995 8.003l5.001 4h-14l5-4-.001.01.001-.009s.938-4.001-3.999-4.001h-1s-4 0-4 3v17.005000000000003h-.001z"
-                        ></path>
-                      </svg>
+                      <img src="assets/img/icon/os-orange.png" />
                     </span>
                     <span class="property-info-entry">
                       <span class="property-info-label">Bathrooms</span>
@@ -154,20 +119,19 @@ const AllCart = () => {
                     </span>
                   </div>
 
-                  <div class="col-xs-3 col-sm-3 col-md-3 p-b-15">
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
                     <span class="property-info-icon icon-garage">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 48 48"
-                      >
-                        <path
-                          class="meta-icon"
-                          fill="#FFA500"
-                          d="M44 0h-40c-2.21 0-4 1.791-4 4v44h6v-40c0-1.106.895-2 2-2h31.999c1.106 0 2.001.895 2.001 2v40h6v-44c0-2.209-1.792-4-4-4zm-36 8.001h31.999v2.999h-31.999zm0 18h6v5.999h-2c-1.104 0-2 .896-2 2.001v6.001c0 1.103.896 1.998 2 1.998h2v2.001c0 1.104.896 2 2 2s2-.896 2-2v-2.001h11.999v2.001c0 1.104.896 2 2.001 2 1.104 0 2-.896 2-2v-2.001h2c1.104 0 2-.895 2-1.998v-6.001c0-1.105-.896-2.001-2-2.001h-2v-5.999h5.999v-3h-31.999v3zm8 12.999c-1.104 0-2-.895-2-1.999s.896-2 2-2 2 .896 2 2-.896 1.999-2 1.999zm10.5 2h-5c-.276 0-.5-.225-.5-.5 0-.273.224-.498.5-.498h5c.275 0 .5.225.5.498 0 .275-.225.5-.5.5zm1-2h-7c-.275 0-.5-.225-.5-.5s.226-.499.5-.499h7c.275 0 .5.224.5.499s-.225.5-.5.5zm-6.5-2.499c0-.276.224-.5.5-.5h5c.275 0 .5.224.5.5s-.225.5-.5.5h-5c-.277 0-.5-.224-.5-.5zm11 2.499c-1.104 0-2.001-.895-2.001-1.999s.896-2 2.001-2c1.104 0 2 .896 2 2s-.896 1.999-2 1.999zm0-12.999v5.999h-16v-5.999h16zm-24-13.001h31.999v3h-31.999zm0 5h31.999v3h-31.999z"
-                        ></path>
-                      </svg>
+                      <img src="assets/img/icon/room-orange.png" />
+                    </span>
+                    <span class="property-info-entry">
+                      <span class="property-info-label">Garages</span>
+                      <span class="property-info-value">2</span>
+                    </span>
+                  </div>
+
+                  <div class="col-xs-6 col-sm-3 col-md-3 p-b-15">
+                    <span class="property-info-icon icon-garage">
+                      <img src="assets/img/icon/shawer-orange.png" />
                     </span>
                     <span class="property-info-entry">
                       <span class="property-info-label">Garages</span>
@@ -287,6 +251,49 @@ const AllCart = () => {
                     </a>
                   </div>
                 </div>
+
+                <div class="section property-share">
+                  <h4 class="s-property-title">Share width your friends</h4>
+                  <div class="roperty-social">
+                    <ul>
+                      <li>
+                        <a title="Share this on dribbble " href="/">
+                          <img src="assets/img/social_big/dribbble_grey.png" alt="title" />
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on facebok " href="/">
+                          <img src="assets/img/social_big/facebook_grey.png" alt="title" />
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on delicious " href="/">
+                          <img src="assets/img/social_big/delicious_grey.png" alt="title" />
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on tumblr " href="/">
+                          <img src="assets/img/social_big/tumblr_grey.png"  alt="title"/>
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on digg " href="/">
+                          <img src="assets/img/social_big/digg_grey.png"  alt="title"/>
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on twitter " href="/">
+                          <img src="assets/img/social_big/twitter_grey.png" alt="title" />
+                        </a>
+                      </li>
+                      <li>
+                        <a title="Share this on linkedin " href="/">
+                          <img src="assets/img/social_big/linkedin_grey.png"  alt="title"/>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
 
@@ -297,32 +304,33 @@ const AllCart = () => {
                     <div class="inner-wrapper">
                       <div class="clear">
                         <div class="col-xs-4 col-sm-4 dealer-face">
-                          <a href="">
+                          <a href="/">
                             <img
                               src="assets/img/client-face1.png"
                               class="img-circle"
+                              alt="title"
                             />
                           </a>
                         </div>
                         <div class="col-xs-8 col-sm-8">
                           <h3 class="dealer-name">
-                            <a href="">Nathan James</a>
+                            <a href="/">Nathan James</a>
                             <span>Real Estate Agent</span>
                           </h3>
                           <div class="dealer-social-media">
-                            <a class="twitter" target="_blank" href="">
+                            <a class="twitter" target="_blank" href="/">
                               <i class="fa fa-twitter"></i>
                             </a>
-                            <a class="facebook" target="_blank" href="">
+                            <a class="facebook" target="_blank" href="/">
                               <i class="fa fa-facebook"></i>
                             </a>
-                            <a class="gplus" target="_blank" href="">
+                            <a class="gplus" target="_blank" href="/">
                               <i class="fa fa-google-plus"></i>
                             </a>
-                            <a class="linkedin" target="_blank" href="">
+                            <a class="linkedin" target="_blank" href="/">
                               <i class="fa fa-linkedin"></i>
                             </a>
-                            <a class="instagram" target="_blank" href="">
+                            <a class="instagram" target="_blank" href="/">
                               <i class="fa fa-instagram"></i>
                             </a>
                           </div>
@@ -361,7 +369,7 @@ const AllCart = () => {
                       <li>
                         <div class="col-md-3 col-sm-3 col-xs-3 blg-thumb p0">
                           <a href="single.html">
-                            <img src="assets/img/demo/small-property-2.jpg" />
+                            <img src="assets/img/demo/small-property-2.jpg" alt="title"/>
                           </a>
                           <span class="property-seeker">
                             <b class="b-1">A</b>
@@ -378,7 +386,7 @@ const AllCart = () => {
                       <li>
                         <div class="col-md-3 col-sm-3 col-xs-3 blg-thumb p0">
                           <a href="single.html">
-                            <img src="assets/img/demo/small-property-1.jpg" />
+                            <img src="assets/img/demo/small-property-1.jpg"  alt="title"/>
                           </a>
                           <span class="property-seeker">
                             <b class="b-1">A</b>
@@ -395,7 +403,7 @@ const AllCart = () => {
                       <li>
                         <div class="col-md-3 col-sm-3 col-xs-3 blg-thumb p0">
                           <a href="single.html">
-                            <img src="assets/img/demo/small-property-3.jpg" />
+                            <img src="assets/img/demo/small-property-3.jpg" alt="title" />
                           </a>
                           <span class="property-seeker">
                             <b class="b-1">A</b>
@@ -413,7 +421,7 @@ const AllCart = () => {
                       <li>
                         <div class="col-md-3 col-sm-3 col-xs-3 blg-thumb p0">
                           <a href="single.html">
-                            <img src="assets/img/demo/small-property-2.jpg" />
+                            <img src="assets/img/demo/small-property-2.jpg"  alt="title"/>
                           </a>
                           <span class="property-seeker">
                             <b class="b-1">A</b>
@@ -436,7 +444,7 @@ const AllCart = () => {
                     <h3 class="panel-title">Ads her</h3>
                   </div>
                   <div class="panel-body recent-property-widget">
-                    <img src="assets/img/ads.jpg" />
+                    <img src="assets/img/ads.jpg"  alt="title"/>
                   </div>
                 </div>
 
