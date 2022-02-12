b8119459586dd9231c9053d547dd09289cf147f1
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-02-12 15:19:09 +0500

    All changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import Main from './Pages/main';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Sayyohlar from "./Components/Sayyohlar";
+import Main from "./Pages/main";
 
 function App() {
   return (
     <>
-      <Main/>
+      <BrowserRouter>
+        <Main />
+        <Link to="/sayyohlar">Sayyohlar</Link>
+        <Routes>
+          <Route path="/sayyohlar" element={Sayyohlar} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
