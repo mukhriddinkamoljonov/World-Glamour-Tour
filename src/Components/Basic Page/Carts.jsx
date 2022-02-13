36057e8aea608c2fec97c2e479e6c497df29114e
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-02-13 22:08:44 +0500

    all changed

--- src/Components/Basic Page/Carts.jsx
+++ src/Components/Basic Page/Carts.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 
 const Carts = () => {
+  const [item, setItem] = useState([]);
   return (
     <div
       className="content-area home-area-1 recent-property"
