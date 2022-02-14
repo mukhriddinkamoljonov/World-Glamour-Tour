f5b05de5ae87ee01bfc31f7ce6f9950259e5cfab
Muhriddin Kamoljonov <muhriddinkamoljonov@gmail.com>
2022-02-14 10:23:50 +0500

    api changed

--- src/Components/Basic Page/Carts.jsx
+++ src/Components/Basic Page/Carts.jsx
@@ -1,223 +1,236 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./Main.css";
 
 const Carts = () => {
   const [item, setItem] = useState([]);
+  const [isReady, setIsReady] = useState(null);
 
   useEffect(() => {
     axios
-      .get("https://wgtour.pythonanywhere.com/api/places/1/")
-      .then((res) => {
-        console.log(res);
-        setItem(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+        .get("https://wgtour.pythonanywhere.com/api/places/1")
+        .then((res) => {
+          setItem(res.data);
+          setIsReady(true)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   }, []);
 
-  // console.log(item.images.file[0]);
   return (
-    <div
-      className="content-area home-area-1 recent-property"
-      style={{ backgroundColor: "#fcfcfc", paddingBottom: " 55px" }}
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-md-10 col-md-offset-1 col-sm-12 text-center page-title">
-            <h2>Mashhur manzillarga maxsus takliflar!</h2>
-            <p>
-              Eʼtiborga molik joylarni,{" "}
-              <span style={{ color: "orange" }}>World Glamour Tour</span> da
-              topishingiz mumkin!
-            </p>
+      <div
+          className="content-area home-area-1 recent-property"
+          style={{backgroundColor: "#fcfcfc", paddingBottom: " 55px"}}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 col-md-offset-1 col-sm-12 text-center page-title">
+              <h2>Mashhur manzillarga maxsus takliflar!</h2>
+              <p>
+                Eʼtiborga molik joylarni,{" "}
+                <span style={{color: "orange"}}>World Glamour Tour</span> da
+                topishingiz mumkin!
+              </p>
+            </div>
           </div>
-        </div>
+          {
+            isReady
+                ? <div className="row">
+                  <div className="proerty-th">
 
-        <div className="row">
-          <div className="proerty-th">
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-1.html">
-                    <img src={item.images} alt="" />
-                    {/* <img src="assets/img/demo/property-1.jpg" alt="title" /> */}
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-1.html">{item.name} </a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-1.html">
+                            {
+                              item.images.map((image) => {
+                                return <img src={image.file} alt={""}/>;
+                              })
+                            }
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-1.html">{item.name} </a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right">1 000,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">1 000,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-2.html">
-                    <img src="assets/img/demo/property-2.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-2.html">Samarqand</a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-2.html">
+                            <img src="assets/img/demo/property-2.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-2.html">Samarqand</a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right">1 200,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">1 200,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-3.html">
-                    <img src="assets/img/demo/property-3.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-3.html">Buxoro</a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-3.html">
+                            <img src="assets/img/demo/property-3.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-3.html">Buxoro</a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right">1 300,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">1 300,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-1.html">
-                    <img src="assets/img/demo/property-4.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-1.html">Xiva</a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-1.html">
+                            <img src="assets/img/demo/property-4.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-1.html">Xiva</a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right">1 000,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">1 000,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-3.html">
-                    <img src="assets/img/demo/property-2.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-3.html">Qo'qon </a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-3.html">
+                            <img src="assets/img/demo/property-2.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-3.html">Qo'qon </a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right">800,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">800,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-2.html">
-                    <img src="assets/img/demo/property-4.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-2.html">Rishton </a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-2.html">
+                            <img src="assets/img/demo/property-4.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-2.html">Rishton </a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
-                    </button>{" "}
+                    </button>
+                            {" "}
                   </span>
-                  <span className="proerty-price pull-right"> 300,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right"> 300,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-two proerty-item">
-                <div className="item-thumb">
-                  <a href="property-1.html">
-                    <img src="assets/img/demo/property-3.jpg" alt="title" />
-                  </a>
-                </div>
-                <div className="item-entry overflow">
-                  <h5>
-                    <a href="property-1.html">Qarshi</a>
-                  </h5>
-                  <div className="dot-hr"></div>
-                  <span className="pull-left">
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-two proerty-item">
+                        <div className="item-thumb">
+                          <a href="property-1.html">
+                            <img src="assets/img/demo/property-3.jpg" alt="title"/>
+                          </a>
+                        </div>
+                        <div className="item-entry overflow">
+                          <h5>
+                            <a href="property-1.html">Qarshi</a>
+                          </h5>
+                          <div className="dot-hr"></div>
+                          <span className="pull-left">
                     <button type="submit" className="btn-grad">
                       Batafsil
                     </button>
                   </span>
-                  <span className="proerty-price pull-right">400,000</span>
-                </div>
-              </div>
-            </div>
+                          <span className="proerty-price pull-right">400,000</span>
+                        </div>
+                      </div>
+                    </div>
 
-            <div className="col-sm-6 col-md-3 p0">
-              <div className="box-tree more-proerty text-center">
-                <div className="item-tree-icon">
-                  <i className="fa fa-th"></i>
+                    <div className="col-sm-6 col-md-3 p0 card-image1">
+                      <div className="box-tree more-proerty text-center">
+                        <div className="item-tree-icon">
+                          <i className="fa fa-th"></i>
+                        </div>
+                        <div className="more-entry overflow">
+                          <h5>
+                            <a href="/">Covid-2019 </a>
+                          </h5>
+                          <h5 className="tree-sub-ttl">Muhim ma'lumotlar va jadval</h5>
+                          <a target="_blank" href="https://www.worldometers.info/coronavirus/">
+                            <button
+                                className="btn border-btn more-black"
+                                value="All properties"
+                            >
+                              Batafsil
+                            </button>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="more-entry overflow">
-                  <h5>
-                    <a href="/">Covid-2019 </a>
-                  </h5>
-                  <h5 className="tree-sub-ttl">Muhim ma'lumotlar va jadval</h5>
-                  <a target="_blank" href="https://www.worldometers.info/coronavirus/">
-                    <button
-                        className="btn border-btn more-black"
-                        value="All properties"
-                    >
-                      Batafsil
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+                : ""
+          }
         </div>
       </div>
-    </div>
   );
-};
+}
 
 export default Carts;
--- src/Components/Basic Page/Footer.jsx
+++ src/Components/Basic Page/Footer.jsx
@@ -24,14 +24,14 @@ const Footer = () => {
                 </h6>
                 <ul className="footer-adress">
                   <li>
-                    <i className="pe-7s-map-marker strong"> </i> 9089 your
-                    adress her
+                    <i className="pe-7s-map-marker strong"> </i> Farg'ona shahar, Birodarlik 28 uy
+
                   </li>
                   <li>
                     <i className="pe-7s-mail strong"> </i> rnj-787@mail.ru
                   </li>
                   <li>
-                    <i className="pe-7s-call strong"> </i> +99897 214 44 01
+                    <i className="pe-7s-call strong"> </i> (998) 97 214 44 01
                   </li>
                 </ul>
               </div>
@@ -76,11 +76,31 @@ const Footer = () => {
                           alt="title"
                         />
                       </a>
-                      <span className="blg-date">12-12-2016</span>
+                      <span className="blg-date">5-01-2021</span>
                     </div>
                     <div className="col-md-8 col-sm-8 col-xs-8 blg-entry">
                       <h6>
-                        <a href="single.html">Biz bilan bog'lanish </a>
+                        <a href="single.html">Chorvoq </a>
+                      </h6>
+                      <p style={{ lineHeight: "17px", padding: "8px 2px" }}>
+                        Lorem ipsum dolor sit amet, nulla ...
+                      </p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="col-md-3 col-sm-4 col-xs-4 blg-thumb p0">
+                      <a href="single.html">
+                        <img
+                          src="assets/img/demo/small-proerty-2.jpg"
+                          alt="title"
+                        />
+                      </a>
+                      <span className="blg-date">12-01-2022</span>
+                    </div>
+                    <div className="col-md-8 col-sm-8 col-xs-8 blg-entry">
+                      <h6>
+                        <a href="single.html">Shohimardon</a>
                       </h6>
                       <p style={{ lineHeight: "17px", padding: "8px 2px" }}>
                         Lorem ipsum dolor sit amet, nulla ...
@@ -100,27 +120,7 @@ const Footer = () => {
                     </div>
                     <div className="col-md-8 col-sm-8 col-xs-8 blg-entry">
                       <h6>
-                        <a href="single.html">Add news functions </a>
-                      </h6>
-                      <p style={{ lineHeight: "17px", padding: "8px 2px" }}>
-                        Lorem ipsum dolor sit amet, nulla ...
-                      </p>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className="col-md-3 col-sm-4 col-xs-4 blg-thumb p0">
-                      <a href="single.html">
-                        <img
-                          src="assets/img/demo/small-proerty-2.jpg"
-                          alt="title"
-                        />
-                      </a>
-                      <span className="blg-date">12-12-2016</span>
-                    </div>
-                    <div className="col-md-8 col-sm-8 col-xs-8 blg-entry">
-                      <h6>
-                        <a href="single.html">Add news functions </a>
+                        <a href="single.html">Amirsoy</a>
                       </h6>
                       <p style={{ lineHeight: "17px", padding: "8px 2px" }}>
                         Lorem ipsum dolor sit amet, nulla ...
